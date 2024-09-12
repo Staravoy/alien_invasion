4d87eb71b04a89c8d92a319af784270373cd6bd8
Staravoy <85824511+Staravoy@users.noreply.github.com>
2024-09-12 21:06:00 +0300

    Готова гра без табло

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,9 @@
 import sys
+from time import sleep
 import pygame
 from settings import Settings
+from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -17,18 +20,27 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
+        # Створити екземпляр для збереження ігрової статистики
+        self.stats = GameStats(self)
+
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()  # Заміна self.alien на self.aliens
 
         self._create_fleet()
 
+        # Створити кнопку Play
+        self.play_button = Button(self, "Play")
+
     def run_game(self):
         """Розпочати головний цикл гри """
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
     def _update_bullets(self):
@@ -39,6 +51,21 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """Реакція на зіткнення куль з прибульцями."""
+        # Видалити усі кулі та прибульців, що зіткнулися
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True
+        )
+
+        if not self.aliens:
+            # Знищити наявні кулі та створити новий флот
+            self.bullets.empty()
+            self._create_fleet()
+            self.settings.increase_speed()
+
     def _check_events(self):
         """Слідкувати за подіями миші та клавіатури"""
         for event in pygame.event.get():
@@ -48,7 +75,16 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
 
+    def _check_play_button(self, mouse_pos):
+        """Розпочати нову гру, коли користувач натисне кнопку Play"""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            # Анолювати статистику гри
+            self.settings.initialize_dynamic_settings()
+            self.stats.game_active = True
     def _check_keydown_events(self, event):
         """Реагування на натискання клавіші"""
         if event.key == pygame.K_RIGHT:
@@ -82,13 +118,113 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)  # Оновлене посилання на групу прибульців
+
+        # Draw button "Play" if game not activate
+        if not self.stats.game_active:
+            self.play_button.draw_button()
         pygame.display.flip()
 
     def _create_fleet(self):
         """Створити флот прибульців"""
-        # Створити прибульця
+        # Створити прибульців та визначити кількість прибульців у ряду.
+        # Відстань між прибульцями дорівнює ширині одного прибульця.
         alien = Alien(self)
-        self.aliens.add(alien)  # Оновлене посилання на групу прибульців
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Визначити, яка кількість рядів прибульців поміщається на екрані
+        ship_height = self.ship.rect.height  # Виправлено
+        available_space_y = (self.settings.screen_height) - (3 * alien_height) - ship_height
+        number_rows = available_space_y // (2 * alien_height)
+
+        # Створити повний флот прибульців.
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)  # Виправлено
+
+    def _create_alien(self, alien_number, row_number):
+        """Створити прибульця та поставити його до ряду."""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number  # Додано ряд прибульців
+        self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """Реагує відповідно до того, чи досяг котрийсь із прибульців краю екрана."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    def _change_fleet_direction(self):
+        """Спуск всього флоту та зміна його напрямку"""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+    def _update_aliens(self):
+        """
+        Перевірити, чи флот знаходиться на краю,
+        тоді оновити позиції всіх прибульців флоту
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
+
+        # Шукати зіткнення із прибульцями.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            self._ship_hit()
+
+        # Шукати, чи котрийсь із прибульців досяг нижнього краю екрана.
+        self._check_aliens_bottom()
+
+    def _ship_hit(self):
+        """Реагування на зіткнення прибульця з кораблем"""
+        if self.stats.ships_left > 0:
+            # Зменшення ship_left
+            self.stats.ships_left -= 1
+
+            # Позбавитися надлишку прибульців та куль
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Створити новий флот та відцентрувати корабель.
+            self._create_fleet()
+            self.ship.center_ship()
+
+            # Пауза
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
+            pygame.mouse.set_visible(True)
+
+    def _check_aliens_bottom(self):
+        """Перевірити, чи не досяг якийсь прибулець нижнього краю екрана"""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Зреагувати так, ніби корабель було підбито.
+                self._ship_hit()
+                break
+    def _check_play_button(self, mouse_pos):
+        """Розпочати нову гру, коли користувач натисне кнопку Play"""
+        button_clecked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clecked and not self.stats.game_active:
+            # Аналювати ігрову статистику
+            self.stats.reset_stats()
+            self.stats.game_active = True
+
+            # Позбавитися надлишку прибульців та куль.
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Створити новий флот та відцентрувати корабель
+            self._create_fleet()
+            self.ship.center_ship()
+
+            # Приховати курсор миші
+            pygame.mouse.set_visible(False)
+
 
 if __name__ == '__main__':
     # Створити екземпляр гри та запустити гру
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -2,7 +2,7 @@ class Settings:
     """Клас для збереження всіх налаштувань гри"""
 
     def __init__(self):
-        """Ініціалізувати налаштування гри."""
+        """Ініціалізувати постійні налаштування гри."""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 700
@@ -10,10 +10,35 @@ class Settings:
 
         # Налаштування коробля
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # Налаштування кулі
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # Налаштування прибульця
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction 1 означає напрямок руху праворуч; -1 ліворуч.
+        self.fleet_direction = 1
+        # Як швидко гра має просуватися
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Ініціалізація змінних налаштувань"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleet_direction 1 представляє напрямок праворуч; -1 -- ліворуч
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Збільшення налаштувань швидкості"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
